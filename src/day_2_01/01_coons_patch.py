a5ecbc5225159851b6d2b58912af10a64b3fe1a1
Rippmann <32359552+Rippmann@users.noreply.github.com>
2018-09-20 21:29:54 +0200

    update

---
+++ src/day_2_01/01_coons_patch.py
@@ -0,0 +1,27 @@
+import rhinoscriptsyntax as rs
+
+from compas.geometry import discrete_coons_patch
+
+from compas.utilities import geometric_key
+
+from compas.datastructures import Mesh
+
+from compas_rhino.artists.meshartist import MeshArtist
+
+if __name__ == '__main__':
+    
+    
+    div = 10
+    
+    crvs = rs.GetObjects("Select four curves (in order)", 4)
+
+    sets_pts = [rs.DivideCurve(crv, div) for crv in crvs]
+
+    ab, bc, dc, ad = sets_pts
+    points, faces = discrete_coons_patch(ab, bc, dc, ad)
+
+    mesh = Mesh.from_vertices_and_faces(points, faces)
+    
+    artist = MeshArtist(mesh, layer='coons_mesh')
+    artist.draw()
+
---
+++ src/day_2_01/03_coons_patch.py
@@ -0,0 +1,63 @@
+import rhinoscriptsyntax as rs
+
+from compas.geometry import discrete_coons_patch
+
+from compas.utilities import geometric_key
+
+from compas.datastructures import Mesh
+
+from compas_rhino.artists.meshartist import MeshArtist
+from compas_rhino import get_line_coordinates
+
+
+def sort_pts(sets_pts):
+    # sorts point sets for coons patch
+
+    # sort point sets in a "continous chain"
+    start_pt = sets_pts[0][-1]
+    new_sets_pts = [sets_pts[0]]
+    sets_pts.pop(0)
+    
+    while sets_pts:
+        for i, set_pts in enumerate(sets_pts):
+            #alternative for a more "forgiving" point comparison
+            #geometric_key(set_pts[0]) == geometric_key(start_pt)
+            if set_pts[0] == start_pt:
+                start_pt = set_pts[-1]
+                new_sets_pts.append(set_pts)
+                sets_pts.pop(i)
+            elif set_pts[-1] == start_pt:
+                start_pt = set_pts[0]
+                set_pts.reverse()
+                new_sets_pts.append(set_pts)
+                sets_pts.pop(i)
+                
+    #coons patch sorting
+    new_sets_pts[2].reverse()
+    new_sets_pts[3].reverse()
+                
+    return new_sets_pts
+
+
+if __name__ == '__main__':
+    
+    
+    div = 20
+    
+    crvs = rs.GetObjects("Select four curves (in order)", 4)
+    lines = get_line_coordinates(crvs)
+    mesh = Mesh.from_lines(lines, delete_boundary_face=True)
+
+#
+#    sets_pts = [rs.DivideCurve(crv, div) for crv in crvs]
+#
+#    sets_pts = sort_pts(sets_pts)
+#
+#    ab, bc, dc, ad = sets_pts
+#    points, faces = discrete_coons_patch(ab, bc, dc, ad)
+
+#    mesh = Mesh.from_vertices_and_faces(points, faces)
+    
+    artist = MeshArtist(mesh, layer='coons_mesh')
+    artist.draw()
+
